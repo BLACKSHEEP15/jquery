feca132ffc852f78e8c6009f63d0a9ee8537b213
sheep <sheep.idea.studio@gmail.com>
2023-01-11 21:42:24 +0800

    20230111

---
+++ 1-1-1-event/js/index.js
@@ -0,0 +1,10 @@
+$('h1').on('click', getTitle);
+$('p').on('click', getParagraph);
+
+function getTitle() {
+    alert('點到標題一');
+}
+
+function getParagraph() {
+    alert('點到段落')
+}
---
+++ 1-1-2-event/js/index.js
@@ -0,0 +1,7 @@
+$('h1').click(function(){
+    alert('標題一')
+})
+
+$('p').click(function(){
+    alert('段落')
+})
